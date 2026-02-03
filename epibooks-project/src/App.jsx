d0bf9e9b96ad5ac69acf56dd5808fbaa84869031
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-02-03 14:53:06 +0100

    First commit  + myNav setted

---
+++ epibooks-project/src/App.jsx
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css"; // import Bootstrap stylesheet
+import MyNav from "./components/MyNav";
+
+function App() {
+  return (
+    <>
+      <MyNav></MyNav>
+      {/* <Welcome></Welcome>
+      <AllTheBooks></AllTheBooks>
+      <MyFooter></MyFooter> */}
+    </>
+  );
+}
+
+export default App;
